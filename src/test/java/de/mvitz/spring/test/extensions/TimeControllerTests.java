b3b611c922da01b79f17e3b578d9cdf767753636
Michael Vitz <michael.vitz@innoq.com>
2019-07-29 14:34:04 +0200

    Use JUnit5 extensions for current date time

--- src/test/java/de/mvitz/spring/test/extensions/TimeControllerTests.java
+++ src/test/java/de/mvitz/spring/test/extensions/TimeControllerTests.java
@@ -3,14 +3,7 @@ package de.mvitz.spring.test.extensions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
-
-import java.time.Clock;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
@@ -19,7 +12,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
-@TestPropertySource(properties = "spring.main.allow-bean-definition-overriding=true")
+@WithLocalDateTime(date = "2019-07-29", time = "14:10:53")
 public class TimeControllerTests {
 
     @Autowired
@@ -30,14 +23,5 @@ public class TimeControllerTests {
         mvc.perform(get("/time"))
             .andExpect(status().isOk())
             .andExpect(content().string(is(equalTo("2019-07-29 14:10:53"))));
-    }
-
-    @TestConfiguration
-    static class TestClockConfiguration {
-        @Bean
-        public Clock clock() {
-            LocalDateTime localDateTime = LocalDateTime.of(2019, 7, 29, 14, 10, 53);
-            return Clock.fixed(localDateTime.toInstant(ZoneOffset.UTC), ZoneOffset.UTC);
-        }
     }
 }
